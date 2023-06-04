4399c52a46a26633025f4e704daa69a376bf6acb
Anna Khizhnyakova <anna.khizhnyakova@gmail.com>
2023-06-04 19:27:00 +0300

    connect to db

--- cypress/e2e/01_API_sighup.cy.js
+++ cypress/e2e/01_API_sighup.cy.js
@@ -5,14 +5,27 @@ describe('API.Auth.Register a new user', () =>{
     let token; 
     const baseURL = Cypress.env('testAPI_url')
 
+    beforeEach(()=>{
+        cy.fixture('sighup.json').then((fixtureData) =>{
+            cy.task('connectDB', 'SELECT id, application_user_role, email, "locked", "name", "password", photo FROM public.application_user;')
+              .then((dbResponse) => {
+                const existingUser = dbResponse.find((user) => user.email === fixtureData.email && user.name === fixtureData.name);
+                if (existingUser) {
+                    cy.log('User exist')
+                    Cypress.runner.stop();
+                }            
+        })
+    })
+
     it('API. Send a POST request to register a new user', ()=> {
         cy.fixture('sighup.json').then((fixtureData)=>{
-            cy.request('POST', baseURL + '/api/v1/auth/register', fixtureData)
-              .then((response)=>{
-                expect(response.status).to.eq(201);
-                expect(response.body.data.authenticationResponse.token).to.exist;
-                token = response.body.data.authenticationResponse.token;
-                cy.log(token)
+                cy.request('POST', baseURL + '/api/v1/auth/register', fixtureData)
+                  .then((response)=>{
+                    expect(response.status).to.eq(201);
+                    expect(response.body.data.authenticationResponse.token).to.exist;
+                    token = response.body.data.authenticationResponse.token;
+                    cy.log(token)
+                  })
             }) 
         })
        
@@ -49,7 +62,7 @@ describe('API.Auth.Register a new user', () =>{
         })
     })
 
-    it('API. Delete use', ()=>{
+    it('API. Delete user', ()=>{
         cy.request({
             method: 'DELETE',
             url: baseURL + '/api/v1/client/settings/account-delete',
